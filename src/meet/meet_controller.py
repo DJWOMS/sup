5426caeaf29df4e0977d6a593092da4d64a227c1
StoneInfection <jokjoli777@gmail.com>
2023-12-21 12:34:16 +0300

    creat meet

--- src/meet/meet_controller.py
+++ src/meet/meet_controller.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
 from src.meet.dependencies.services import IMeetService
-from src.meet.meet_dto import CreateUserMeet
+from src.meet.meet_dto import CreateUserMeet, MeetBase
 
 router = APIRouter(prefix="/meet", tags=["meet"])
 
 
-@router.post("/user_meet", response_model=CreateUserMeet)
+@router.post("/user_meet", response_model=MeetBase)
 async def user_meet(dto: CreateUserMeet, service: IMeetService):
     return await service.create(dto)
