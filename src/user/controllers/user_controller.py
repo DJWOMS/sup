0cec5980af1af5991ae8e4812fa8752d3a7937de
Omelchenko Michael <socanime@gmail.com>
2024-01-26 06:28:28 +0300

    fix user controller

--- src/user/controllers/user_controller.py
+++ src/user/controllers/user_controller.py
@@ -1,41 +1,36 @@
 from fastapi import APIRouter
 
 from ..dependencies.services import IUserService
-from src.user.dtos.user_dto import CreateUser, ResponseUser, ResponseUserList, UpdateUser, UpdatePassword
-from fastapi import APIRouter, Query, Path
-
-from ..dependencies.services import IUserService
-from src.user.dtos.user_dto import CreateUser, ResponseUser, ResponseUserList
+from ..user_dto import CreateUserDTO, GetUserDTO, GetUserListDTO, UpdateUserDTO, UpdatePasswordDTO
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 
-@router.post("/", response_model=ResponseUser)
-async def create_user(dto: CreateUser, service: IUserService):
+@router.post("/", response_model=GetUserDTO)
+async def create_user(dto: CreateUserDTO, service: IUserService):
     return await service.create(dto)
 
 
-@router.put("/{pk}", response_model=ResponseUser)
-async def update_user(pk: int, dto: UpdateUser, service: IUserService):
-    return await service.update(pk, dto)
+@router.get("/", response_model=list[GetUserListDTO])
+async def get_list_users(service: IUserService, limit: int = 50):
+    return await service.get_list(limit)
 
 
-@router.put("/password/{pk}", response_model=UpdatePassword)
-async def update_pass(pk: int, dto: UpdatePassword, service: IUserService):
-    return await service.update_pass(pk, dto)
-
-
-@router.delete("/del/{pk}")
-async def delete_message(pk: int, service: IUserService):
-    return await service.delete(pk)
-
-
-@router.get("/user/{pk}", response_model=ResponseUser)
+@router.get("/{pk}", response_model=GetUserDTO)
 async def get_user(pk: int, service: IUserService):
     return await service.get(pk)
 
 
-@router.get("/get_list", response_model=list[ResponseUserList])
-async def get_list_users(service: IUserService, limit: int = 50):
-    return await service.get_list(limit)
+@router.put("/{pk}", response_model=GetUserDTO)
+async def update_user(pk: int, dto: UpdateUserDTO, service: IUserService):
+    return await service.update(pk, dto)
 
+
+@router.delete("/{pk}")
+async def delete_message(pk: int, service: IUserService):
+    return await service.delete(pk)
+
+
+@router.put("/{pk}/change_password", response_model=UpdatePasswordDTO)
+async def update_pass(pk: int, dto: UpdatePasswordDTO, service: IUserService):
+    return await service.update_pass(pk, dto)
