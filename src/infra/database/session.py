594dd3aaf4289a9642ae1aad2ce5952b44677952
Omelchenko Michael <socanime@gmail.com>
2024-01-27 12:06:28 +0300

    move database config to infra

--- src/infra/database/session.py
+++ src/infra/database/session.py
@@ -2,6 +2,6 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.config.database.db_helper import db_helper
+from .db_helper import db_helper
 
 ISession = Annotated[AsyncSession, Depends(db_helper.get_session)]
