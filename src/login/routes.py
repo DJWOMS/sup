9fe1202830268157e789b4835aeffc3b0ebce213
Omelchenko Michael <socanime@gmail.com>
2024-01-28 18:15:18 +0300

    Удалил лишние файлы

--- src/login/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from ..login import login_controller
-
-
-def get_context_router():
-    router = APIRouter()
-    router.include_router(login_controller.router)
-    return router
--- src/routes.py
+++ src/routes.py
@@ -1,21 +1,23 @@
 from fastapi import APIRouter
 
-from .email import routes as email_controller
-from .user import routes as user_routes
-from .invitation import routes as invitation_routes
-from .meet import routes as meet_routes
-from .login import routes as login_controller
-from .permission import routes as permission_controller
-from .role import routes as role_controller
+from .api import (
+    email_controller,
+    invitation_controller,
+    login_controller,
+    meet_controller,
+    permission_controller,
+    role_controller,
+    user_controller,
+)
 
 
 def get_apps_router():
     router = APIRouter()
-    router.include_router(user_routes.get_context_router())
-    router.include_router(invitation_routes.get_context_router())
-    router.include_router(meet_routes.get_context_router())
-    router.include_router(email_controller.get_context_router())
-    router.include_router(login_controller.get_context_router())
-    router.include_router(permission_controller.get_context_router())
-    router.include_router(role_controller.get_context_router())
+    router.include_router(permission_controller.router)
+    router.include_router(role_controller.router)
+    router.include_router(user_controller.router)
+    router.include_router(email_controller.router)
+    router.include_router(invitation_controller.router)
+    router.include_router(login_controller.router)
+    router.include_router(meet_controller.router)
     return router
