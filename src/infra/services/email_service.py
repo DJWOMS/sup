d3b5c0b2c03d3ca2e3f68088616fa87179f93d2d
jj2 <asetovrauan95@gmail.com>
2024-02-06 20:46:16 +0600

    added base class for sending mail

---
+++ src/infra/services/email_service.py
@@ -0,0 +1,37 @@
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class EmailService:
+    """Basic Service to send emails"""
+    def __init__(self):
+        # Credentials
+
+        self.user: str = os.getenv("EMAIL_USER")
+        self.password: str = os.getenv("EMAIL_PASSWORD")
+
+        # Server config
+        self.smtp_port = 465
+        self.smtp_server = "smtp.gmail.com"
+        self.smtp_context = ssl.create_default_context()
+
+    def send_email(self, recipient_email: str, subject: str, body: str) -> None:
+
+        # Message config
+        message = MIMEText(body)
+        message["Subject"] = subject
+        message["From"] = self.user
+        message['To'] = recipient_email
+
+        # Message sending
+        with smtplib.SMTP_SSL(self.smtp_server, self.smtp_port, context=self.smtp_context) as smtp:
+            smtp.login(self.user, self.password)
+            smtp.sendmail(self.user, recipient_email, message.as_string())
+
+
