26671e353ff01af86430683ff7ae6cc96ee52cc1
Akob <jevahiryantt@gmail.com>
2024-02-11 10:37:26 +0300

    fix migrations flag

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-alembic upgrade head
+if [ "$DB_RUN_AUTO_MIGRATE" = "True" ]; then
+  echo "Running Alembic migrations..."
+  alembic upgrade head
+else
+  echo "Skipping Alembic migrations..."
+fi
 
-uvicorn src.main:app --reload --host "0.0.0.0" --port "8000" --log-level "debug" --use-colors
+uvicorn src.main:app --host "0.0.0.0" --port "8000" --log-level "debug" --reload --use-colors
