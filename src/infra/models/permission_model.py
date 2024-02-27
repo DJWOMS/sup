ee24eefd1ef630046af72faee88a91c495d99109
Vadim <vadimmelnik977@gmail.com>
2024-02-27 22:11:05 +0300

    Refactor PermissionModel

--- src/infra/models/permission_model.py
+++ src/infra/models/permission_model.py
@@ -1,5 +1,7 @@
+from typing import List
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base_model import Base
 
@@ -11,9 +13,15 @@ class PermissionModel(Base):
     :param title: название права доступа
     :param code: код права доступа
     :param description: описание прав доступа
+    :param users: все юзеры, у которых есть это право доступа
     """
     __tablename__ = "permissions"
 
     title: Mapped[str] = mapped_column(String(20))
     code: Mapped[int] = mapped_column(unique=True)
     description: Mapped[str] = mapped_column(nullable=True)
+    users: Mapped[List["UserModel"]] = relationship(
+        back_populates="permissions",
+        secondary="user_permission",
+        lazy="raise_on_sql"
+    )
