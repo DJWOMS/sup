802f692b5636324a86b802290500b36e3318242b
Vadim <vadimmelnik977@gmail.com>
2024-02-27 22:05:43 +0300

    Many-to-Many for User and Permission

---
+++ src/infra/models/user_permission_model.py
@@ -0,0 +1,29 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped
+
+from .base_model import Base
+
+
+class UserPermissionModel(Base):
+    """ Модель разрешений пользователя
+
+    :param id: идентификатор
+    :param user_id: id пользователя
+    :param permission_id: id разрешения
+    :param created_at: дата создания
+    :param updated_at: дата обновления
+    """
+
+    __tablename__ = "user_permission"
+    user_id: Mapped[int] = mapped_column(ForeignKey(
+        "users.id",
+        ondelete="CASCADE",
+    ),
+        primary_key=True
+    )
+    permission_id: Mapped[int] = mapped_column(ForeignKey(
+        "permissions.id",
+        ondelete="CASCADE",
+    ),
+        primary_key=True
+    )
